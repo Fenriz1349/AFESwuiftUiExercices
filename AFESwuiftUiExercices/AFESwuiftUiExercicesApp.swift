2f472c704f94741f0d1b891d0bc826fc833b65d3
Fenriz1349 <129265577+Fenriz1349@users.noreply.github.com>
2024-03-04 16:33:32 +0100

    Initial Commit

---
+++ AFESwuiftUiExercices/AFESwuiftUiExercicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFESwuiftUiExercicesApp.swift
+//  AFESwuiftUiExercices
+//
+//  Created by apprenant68 on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AFESwuiftUiExercicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
